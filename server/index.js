4d904a38b1bf775483e1360332d43cf4dff3fad5
itxsoumya <itxsoumyaranjan@gmail.com>
2023-12-12 10:15:48 +0530

    server added

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors());
 app.use(bodyParser.json())
 require('dotenv').config()
-const port = 3000
+const port = process.env.PORT || 5000
 const db_url = process.env.DB_URL
 const db_user = process.env.DB_USERNAME
 const db_pass = process.env.DB_PASSWORD
